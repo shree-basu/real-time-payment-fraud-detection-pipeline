a414460462936727677ec0a7b0e17fef8b0e859d
Shreetama <basushreetama99@gmail.com>
2026-05-20 19:45:56 +0530

    feat: add test_pipeline.py imports

---
+++ tests/test_pipeline.py
@@ -0,0 +1,7 @@
+import apache_beam as beam
+from apache_beam.testing.test_pipeline import TestPipeline
+from apache_beam.testing.util import assert_that, is_not_empty
+from pipeline.transforms.validate import ValidateTransaction
+from pipeline.transforms.enrich import EnrichTransaction
+from pipeline.transforms.detect import DetectFraud
+import json
